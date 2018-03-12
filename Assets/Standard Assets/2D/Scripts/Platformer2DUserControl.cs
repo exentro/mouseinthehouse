1a5d367780d7e19e046a793910339d0f20ad2564
Tristan Jardon <tristan.jardon@gmail.com>
2018-03-12 17:18:47 +0100

    Fix jump bug

--- Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
+++ Assets/Standard Assets/2D/Scripts/Platformer2DUserControl.cs
@@ -38,7 +38,7 @@ namespace UnityStandardAssets._2D
                 if (!m_Jump)
                 {
                     // Read the jump input in Update so button presses aren't missed.
-                    m_Jump = m_player.GetButton("Jump");
+                    m_Jump = m_player.GetButtonDown("Jump");
                 }
             }
         }
