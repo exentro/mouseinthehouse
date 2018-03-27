14c2e22470e47c915894d06dc2b3a766a43c452d
Tristan Jardon <tristan.jardon@gmail.com>
2018-03-27 16:40:05 +0200

    Change cameras behaviour

--- Assets/_House in the House/Scripts/Camera/Camera2DFollow.cs
+++ Assets/_House in the House/Scripts/Camera/Camera2DFollow.cs
@@ -9,6 +9,7 @@ public class Camera2DFollow : MonoBehaviour
     public Transform m_player2;
     public float m_damping = 0.2f;
     public Camera m_camera2;
+    public Camera m_camera3;
     public AnimationCurve m_animCurve;
 
     #endregion
@@ -19,11 +20,16 @@ public class Camera2DFollow : MonoBehaviour
         m_screenLength = Vector2.Distance(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)), Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)));
         m_camera1 = GetComponent<Camera>();
         m_camera2Follow = m_camera2.GetComponent<Camera2DAuxFollow>();
+        m_camera3Follow = m_camera3.GetComponent<Camera2DAuxFollow>();
+        m_camera2.rect = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
+        m_camera3.rect = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
+        m_focusPlayer1 = m_player1.Find("CameraFocus");
+        m_focusPlayer2 = m_player2.Find("CameraFocus");
     }
     
     private void LateUpdate()
     {
-        float m_deltaX = Math.Abs(m_player1.position.x - m_player2.position.x);
+        float m_deltaX = Math.Abs(m_focusPlayer1.position.x - m_focusPlayer2.position.x);
 
         if (!m_splited && m_deltaX > m_screenLength * .5)
             SplitScreen();
@@ -33,18 +39,20 @@ public class Camera2DFollow : MonoBehaviour
             else
                 m_merging = false;
 
-        if (!m_splited) // 2 Players on same screen
-            SmoothLookPoint(m_camera1, GetMidPoint(m_player1.position, m_player2.position));
-        else if (!m_merging) // 2 players on differents screens
+        // if (!m_splited) // 2 Players on same screen
+            SmoothLookPoint(m_camera1, GetMidPoint(m_focusPlayer1.position, m_focusPlayer2.position));
+        //else 
+        if (!m_merging) // 2 players on differents screens
             SmoothLookEachPlayer();
     }
 
     private void SmoothLookPoint(Camera camera, Vector3 target)
     {
         Vector3 aheadTargetPos = target + Vector3.forward * m_offsetZ;
-        Vector3 newPos = Vector3.SmoothDamp(camera.transform.position, aheadTargetPos, ref m_currentVelocity, m_damping);
-        if(m_splited)
-            newPos.x = Mathf.Min(newPos.x, m_camera2.transform.position.x - m_screenLength * 0.5f);
+        //Vector3 newPos = Vector3.SmoothDamp(camera.transform.position, aheadTargetPos, ref m_currentVelocity, m_damping);
+        Vector3 newPos = Vector3.Lerp(camera.transform.position, aheadTargetPos, m_damping * Time.deltaTime);
+        //if(m_splited)
+        //    newPos.x = Mathf.Min(newPos.x, m_camera2.transform.position.x - m_screenLength * 0.5f);
         camera.transform.position = newPos;
     }
 
@@ -60,16 +68,18 @@ public class Camera2DFollow : MonoBehaviour
     }
 
     private void SmoothLookEachPlayer()
-    {   // Camera1 always follow player on the left
-        if (m_player1.position.x < m_player2.position.x)
+    {   // Camera2 always follow player on the left
+        if (m_focusPlayer1.position.x < m_focusPlayer2.position.x)
         {
-            SmoothLookPoint(m_camera1, m_player1.position);
-            m_camera2Follow.SmoothLookPoint(m_camera2, m_player2.position, m_screenLength, m_camera1);
+            //SmoothLookPoint(m_camera1, m_focusPlayer1.position);
+            m_camera2Follow.SmoothLookPoint(m_camera2, m_focusPlayer1.position, m_screenLength, m_camera3);
+            m_camera3Follow.SmoothLookPoint(m_camera3, m_focusPlayer2.position, m_screenLength, m_camera2);
         }
         else
         {
-            SmoothLookPoint(m_camera1, m_player2.position);
-            m_camera2Follow.SmoothLookPoint(m_camera2, m_player1.position, m_screenLength, m_camera1);
+            //SmoothLookPoint(m_camera1, m_focusPlayer2.position);
+            m_camera2Follow.SmoothLookPoint(m_camera2, m_focusPlayer2.position, m_screenLength, m_camera3);
+            m_camera3Follow.SmoothLookPoint(m_camera3, m_focusPlayer1.position, m_screenLength, m_camera2);
         }
     }
 
@@ -78,56 +88,73 @@ public class Camera2DFollow : MonoBehaviour
         m_splited = true;
         m_merging = false;
         m_camera2.transform.position = m_camera1.transform.position;
-        m_camera1.rect = new Rect(0.0f, 0.0f, 0.5f, 1.0f);
-        m_camera2.rect = new Rect(0.5f, 0.0f, 0.5f, 1.0f);
-        if (m_player1.position.x < m_player2.position.x)
+        m_camera3.transform.position = m_camera1.transform.position;
+        if (m_focusPlayer1.position.x < m_focusPlayer2.position.x)
         {
-            m_camera1.transform.position = new Vector3(m_player1.position.x, m_camera1.transform.position.y, m_camera1.transform.position.z);
-            m_camera2.transform.position = new Vector3(m_player2.position.x, m_camera2.transform.position.y, m_camera2.transform.position.z);
+            m_camera2.transform.position = new Vector3(m_focusPlayer1.position.x, m_camera2.transform.position.y, m_camera2.transform.position.z);
+            m_camera3.transform.position = new Vector3(m_focusPlayer2.position.x, m_camera3.transform.position.y, m_camera3.transform.position.z);
         }
         else
         {
-            m_camera1.transform.position = new Vector3(m_player2.position.x, m_camera1.transform.position.y, m_camera1.transform.position.z);
-            m_camera2.transform.position = new Vector3(m_player1.position.x, m_camera2.transform.position.y, m_camera2.transform.position.z);
+            m_camera2.transform.position = new Vector3(m_focusPlayer2.position.x, m_camera2.transform.position.y, m_camera2.transform.position.z);
+            m_camera3.transform.position = new Vector3(m_focusPlayer1.position.x, m_camera3.transform.position.y, m_camera3.transform.position.z);
         }
+        m_camera1.depth = -1;
     }
 
     private void MergeScreens()
     {
         m_merging = true;
-        float halfDeltaY = Math.Abs(m_player1.position.y - m_player2.position.y) * 0.5f;
+        float halfDeltaY = Math.Abs(m_focusPlayer1.position.y - m_focusPlayer2.position.y) * 0.5f;
+        float xPos1;
+        float xPos2;
 
         Vector3 player1Merge = new Vector3();
         Vector3 player2Merge = new Vector3();
-        if (m_player1.position.y > m_player2.position.y)
+        float xPos = GetMidPoint(m_focusPlayer1.position, m_focusPlayer2.position).x;
+        print(xPos);
+        if (m_focusPlayer1.position.x > m_focusPlayer2.position.x)
         {
-            player1Merge = new Vector3(m_player1.position.x, m_player1.position.y - halfDeltaY, 0f);
-            player2Merge = new Vector3(m_player2.position.x, m_player2.position.y + halfDeltaY, 0f);
+            xPos1 = xPos + m_screenLength * .25f;
+            xPos2 = xPos - m_screenLength * .25f;
         }
         else
         {
-            player1Merge = new Vector3(m_player1.position.x, m_player1.position.y + halfDeltaY, 0f);
-            player2Merge = new Vector3(m_player2.position.x, m_player2.position.y - halfDeltaY, 0f);
+            xPos1 = xPos - m_screenLength * .25f;
+            xPos2 = xPos + m_screenLength * .25f;
+        }
+        if (m_focusPlayer1.position.y > m_focusPlayer2.position.y)
+        {
+            player1Merge = new Vector3(xPos1, m_focusPlayer1.position.y - halfDeltaY, 0f);
+            player2Merge = new Vector3(xPos2, m_focusPlayer2.position.y + halfDeltaY, 0f);
+        }
+        else
+        {
+            player1Merge = new Vector3(xPos1, m_focusPlayer1.position.y + halfDeltaY, 0f);
+            player2Merge = new Vector3(xPos2, m_focusPlayer2.position.y - halfDeltaY, 0f);
         }
 
-        if (m_player1.position.x < m_player2.position.x)
+        if (m_focusPlayer1.position.x < m_focusPlayer2.position.x)
         {
-            SmoothLookPoint(m_camera1, player1Merge);
-            m_camera2Follow.SmoothLookPoint(m_camera2, player2Merge, m_screenLength, m_camera1);
+            //SmoothLookPoint(m_camera2, player1Merge);
+            m_camera2Follow.SmoothLookPoint(m_camera2, player1Merge, m_screenLength, m_camera3);
+            m_camera3Follow.SmoothLookPoint(m_camera3, player2Merge, m_screenLength, m_camera2);
         }
         else
         {
-            SmoothLookPoint(m_camera1, player2Merge);
-            m_camera2Follow.SmoothLookPoint(m_camera2, player1Merge, m_screenLength, m_camera1);
+            //SmoothLookPoint(m_camera1, player2Merge);
+            m_camera2Follow.SmoothLookPoint(m_camera2, player2Merge, m_screenLength, m_camera3);
+            m_camera3Follow.SmoothLookPoint(m_camera3, player1Merge, m_screenLength, m_camera2);
         }
 
         if(Math.Round(m_camera1.transform.position.y, 1) == Math.Round(m_camera2.transform.position.y, 1))
         {
-            m_camera1.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
-            m_camera2.rect = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
-            m_camera1.transform.position = GetMidPoint(m_camera1.transform.position, m_camera2.transform.position);
+            //m_camera1.rect = new Rect(0.0f, 0.0f, 1.0f, 1.0f);
+            //m_camera2.rect = new Rect(0.0f, 0.0f, 0.0f, 0.0f);
+            m_camera1.transform.position = GetMidPoint(m_focusPlayer1.position, m_focusPlayer2.position);
             m_splited = false;
             m_merging = false;
+            m_camera1.depth = 1;
         }
     }
 
@@ -138,9 +165,13 @@ public class Camera2DFollow : MonoBehaviour
 
     private Camera m_camera1;
     private Camera2DAuxFollow m_camera2Follow;
+    private Camera2DAuxFollow m_camera3Follow;
     private float m_screenLength;
     private bool m_splited = false;
     private bool m_merging = false;
+
+    private Transform m_focusPlayer1;
+    private Transform m_focusPlayer2;
 
     #endregion
 
