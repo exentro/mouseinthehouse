2abbe8b7db893835e0e53042c9078c651817d233
Exentro <ixr_exentro@hotmail.com>
2018-04-05 17:27:25 +0200

    wip

---
+++ Assets/_House in the House/Team/François/ToolsTimeScaleInScene.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ToolsTimeScaleInScene : MonoBehaviour
+{
+    [SerializeField] float m_timeScale = 1f;
+    private float previousValue;
+
+	void Start ()
+    {
+        previousValue = Time.timeScale;
+        Time.timeScale = m_timeScale;
+    }
+
+    private void OnDisable()
+    {
+        Time.timeScale = previousValue;
+    }
+}
